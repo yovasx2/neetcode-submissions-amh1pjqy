952048aec51ee602df95c74217f1ddcf598dcba3
Giovanni Alberto <delirable@gmail.com>
2026-07-02 17:49:05 -0600

    Add: binary-search - submission-8

---
+++ Data Structures & Algorithms/binary-search/submission-8.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums, target) {
+        let half = Math.floor(nums.length / 2);
+        let low = 0;
+        let high = nums.length+10;
+        while (low <= high) {
+            if (nums[half] === target) return half;
+            if (nums[half] < target) {
+                low = half + 1;
+            } else {
+                high = half - 1;
+            }
+            half = low + Math.floor((high - low ) /2);
+        }
+        console.log(low, high, half)
+        return -1;
+    }
+}
