68badd82e69e32be9c7dcb7d0b7e579869357ace
Giovanni Alberto <delirable@gmail.com>
2026-07-06 14:07:40 -0600

    Add: kth-largest-integer-in-a-stream - submission-0

---
+++ Data Structures & Algorithms/kth-largest-integer-in-a-stream/submission-0.js
@@ -0,0 +1,80 @@
+class MinHeap {
+    constructor() {
+        this.heap = [];
+    }
+
+    push(val) {
+        this.heap.push(val);
+        this._up(this.heap.length - 1);
+    }
+
+    pop() {
+        if (this.size() === 0) return null;
+        const top = this.heap[0];
+        const bottom = this.heap.pop();
+        if (this.size() > 0) {
+            this.heap[0] = bottom;
+            this._down(0);
+        }
+        return top;
+    }
+
+    peek() {
+        return this.heap[0];
+    }
+
+    size() {
+        return this.heap.length;
+    }
+
+    _up(i) {
+        while (i > 0) {
+            const p = Math.floor((i - 1) / 2);
+            if (this.heap[p] <= this.heap[i]) break;
+            [this.heap[p], this.heap[i]] = [this.heap[i], this.heap[p]];
+            i = p;
+        }
+    }
+
+    _down(i) {
+        const len = this.heap.length;
+        while (i * 2 + 1 < len) {
+            let left = i * 2 + 1;
+            let right = i * 2 + 2;
+            let best = left;
+            if (right < len && this.heap[right] < this.heap[left]) {
+                best = right;
+            }
+            if (this.heap[i] <= this.heap[best]) break;
+            [this.heap[i], this.heap[best]] = [this.heap[best], this.heap[i]];
+            i = best;
+        }
+    }
+}
+
+class KthLargest {
+    /**
+     * @param {number} k
+     * @param {number[]} nums
+     */
+    constructor(k, nums) {
+        this.k = k;
+        this.minHeap = new MinHeap();
+        
+        for (const num of nums) {
+            this.add(num);
+        }
+    }
+
+    /**
+     * @param {number} val
+     * @return {number}
+     */
+    add(val) {
+        this.minHeap.push(val);
+        if (this.minHeap.size() > this.k) {
+            this.minHeap.pop();
+        }
+        return this.minHeap.peek();
+    }
+}
