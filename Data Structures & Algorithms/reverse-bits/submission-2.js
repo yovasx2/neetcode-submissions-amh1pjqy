7a7f5a016e548baecdcdc27f42020cc695d7ced7
Giovanni Alberto <delirable@gmail.com>
2026-07-07 19:29:16 -0600

    Add: reverse-bits - submission-2

---
+++ Data Structures & Algorithms/reverse-bits/submission-2.js
@@ -0,0 +1,14 @@
+class Solution {
+    /**
+     * @param {number} n - a positive integer
+     * @return {number} - a positive integer
+     */
+    reverseBits(n) {
+        let res = 0;
+        for (let i = 0; i < 32; i++) {
+            const bit = (n >> i) & 1;
+            res |= bit << (31 - i);
+        }
+        return res >>> 0;
+    }
+}
