00f33798d1fd3fb9ce0d89636c1de10797470e6f
Giovanni Alberto <delirable@gmail.com>
2026-07-22 18:14:06 -0600

    Add: remove-element - submission-5

---
+++ Data Structures & Algorithms/remove-element/submission-5.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} val
+     * @return {number}
+     */
+    removeElement(nums, val) {
+        let index = 0;
+        for (let i = 0; i < nums.length; i++) {
+            if (nums[i] !== val) {
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+        return index
+    }
+}
