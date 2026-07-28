7d7d2e8dde4f720710b715110510f81c5974a535
Giovanni Alberto <delirable@gmail.com>
2026-07-27 18:57:36 -0600

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.js
@@ -0,0 +1,19 @@
+class Solution {
+    /**
+     * @param {number[]} temperatures
+     * @return {number[]}
+     */
+    dailyTemperatures(temperatures) {
+        const res = new Array(temperatures.length).fill(0)
+        const stack = [];
+        for (let i = 0; i < temperatures.length; i++) {
+            const t = temperatures[i];
+            while (stack.length > 0 && t > stack[stack.length - 1][0]) {
+                const [stackT, stackInd] = stack.pop();
+                res[stackInd] = i - stackInd;
+            }
+            stack.push([t, i]);
+        }
+        return res;
+    }
+}
